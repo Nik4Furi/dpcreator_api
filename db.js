fba1373546451335945e39c09863e37f312f9c27
Nik4Furi <nikhilgurjar2212@gmail.com>
2023-07-03 09:53:50 +0530

    Resolve issue regarding to env and db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 //Connect app with database
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true}). 
 then( ()=> console.log('Connection to database')). 
 catch((e) => console.error('Error occured during connection to database ',e))
